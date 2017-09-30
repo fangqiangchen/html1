bd2c7e8f8e1295b23e10aa6ede88bb04fb6356b8
cfq615@qq.com <zuoan1314>
2017-09-30 14:47:21 +0800

    121
    (cherry picked from commit 8614253f3a0b77d6bbb297937bdb55d796d28884)
    
    # Conflicts:
    #	js8/2.1.html

--- js8/js/script.js
+++ js8/js/script.js
@@ -1,84 +1,106 @@
-/**
- * Created by Administrator on 2017/9/3.
- */
-//封装一个代替getElementById()方法
-function byId(id) {
+var timer = null,
+    index = 0,
+    pics = byId("banner").getElementsByTagName("div"),
+    dots = byId("dots").getElementsByTagName("span"),
+    size = pics.length,
+    prev = byId("prev"),
+    next = byId("next"),
+    menuItems = byId("menu-content").getElementsByTagName("div"),
+    subMenu = byId("sub-menu"),
+    subItems = subMenu.getElementsByClassName("inner-box");
+
+function byId(id){
     return typeof(id)==="string"?document.getElementById(id):id;
 }
-//全局变量
-var index =0,timer=null,
-    pics=byId("banner").getElementsByTagName("div"),
-    dots=byId('dots').getElementsByTagName("span"),
-    prev=byId("prev");
-    next=byId("next");
-    len=pics.length;
-function slideImg() {
-    var main=byId("main");
-    //滑过清楚定时器，离开继续
-    main.onmouseover=function(){
-        //滑过清除定时器
-        if(timer) clearInterval(timer);
-    }
-    main.onmouseout=function () {
-        var timer =setInterval(function () {
-            index++;
-            if(index>=len){
-                index=0;
-            }
-            //切换图片
-            changeImg();
-        },3000);
-    }
-    //自动在mian上触发鼠标离开的事件
-    main.onmouseout();
-    //遍历所有点击，且绑定点击事件，点击圆点切换图片
-    for(var d=0;d<len;d++){
 
-
-        //给所有span添加一个id的属性，值为d作为当前span的索引
-        dots[d].id=d;
-        dots[d].onclick=function(){
-            //改变index为前span的索引
-            index=this.id;
-            this.className="active";
-            changeImg();
-        }
+// 清除定时器,停止自动播放
+function stopAutoPlay(){
+    if(timer){
+        clearInterval(timer);
     }
-    //下一张
-    next.onclick=function(){
+}
+
+// 图片自动轮播
+function startAutoPlay(){
+    timer = setInterval(function(){
         index++;
-        if (index>=len)index=0;
-        changeImg();
-        //上一张
-        prev.onclick=function(){
-            index--;
-            if(index<0)index=len;
-            changeImg()
+        if(index >= size){
+            index = 0;
         }
-
-    }
+        changeImg();
+    },3000)
 }
 
-
-//切换图片
 function changeImg(){
-    //遍历banner下多有的div,将其隐藏
-    for(var i=0;i<len;i++){
-        pics[i].style.display="none";
-        dots[i].className="";
+    for(var i=0,len=dots.length;i<len;i++){
+        dots[i].className = "";
+        pics[i].style.display = "none";
     }
-    //根据index索引找到当前div和当前span,将其显示出来和设为当前
-    pics[index].style.display='block';
-    dots[index].className="active";
+    dots[index].className = "active";
+    pics[index].style.display = "block";
 }
-slideImg();
 
-//切换图片
-function changeImg(){
-    //遍历banner下多有的div,将其隐藏
-    for(var i=0;i<len;i++){
-        pics[i].style.display="none";
+function slideImg(){
+    var main = byId("main");
+    var banner = byId("banner");
+    var menuContent = byId("menu-content");
+    main.onmouseover = function(){
+        stopAutoPlay();
     }
-    pics[index].style.display='block';
+    main.onmouseout = function(){
+        startAutoPlay();
+    }
+    main.onmouseout();
+
+    // 点击导航切换
+    for(var i=0,len=dots.length;i<len;i++){
+        dots[i].id = i;
+        dots[i].onclick = function(){
+            index = this.id;
+            changeImg();
+        }
+    }
+
+    // 下一张
+    next.onclick = function(){
+        index++;
+        if(index>=size) index=0;
+        changeImg();
+    }
+
+    // 上一张
+    prev.onclick = function(){
+        index--;
+        if(index<0) index=size-1;
+        changeImg();
+    }
+
+    // 菜单
+    for(var m=0,mlen=menuItems.length;m<mlen;m++){
+        menuItems[m].setAttribute("data-index",m);
+        menuItems[m].onmouseover = function(){
+            subMenu.className = "sub-menu";
+            var idx = this.getAttribute("data-index");
+            for(var j=0,jlen=subItems.length;j<jlen;j++){
+                subItems[j].style.display = 'none';
+                menuItems[j].style.background = "none";
+            }
+            subItems[idx].style.display = "block";
+            menuItems[idx].style.background = "rgba(0,0,0,0.1)";
+        }
+    }
+
+    subMenu.onmouseover = function(){
+        this.className = "sub-menu";
+    }
+
+    subMenu.onmouseout = function(){
+        this.className = "sub-menu hide";
+    }
+
+    menuContent.onmouseout = function(){
+        subMenu.className = "sub-menu hide";
+    }
 }
+
 slideImg();
